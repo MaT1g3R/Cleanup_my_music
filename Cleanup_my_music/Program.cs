c71c750b9af4de86d780bb692bde27def29d260f
MaT1g3R <982341777@qq.com>
2017-01-19 00:04:29 -0500

    Ran the FileSystem class on my whole music libary and holy shit it's fast.

--- Cleanup_my_music/Program.cs
+++ Cleanup_my_music/Program.cs
@@ -13,7 +13,8 @@ namespace Cleanup_my_music {
             Application.SetCompatibleTextRenderingDefault(false);
             //MessageBox.Show("this is how you debug");
             string testingDir = "C://Users//MaT1g3R//Desktop//Testing Directory";
-            List<string> files = FileSystem.getFiles(testingDir);
+            string myWholeLibrary = "A://Music";
+            List<string> files = FileSystem.getFiles(myWholeLibrary);
             Application.Run(new mainWindow());
             //System.Diagnostics.Debug.Write("Or is this how you debug");
             //Actually tho please use the debugger

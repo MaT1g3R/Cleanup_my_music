9309f4690e7c43e5bbcb9c6eafe4f767426b07f0
MaT1g3R <982341777@qq.com>
2017-01-19 13:41:25 -0500

    3rd Implementation of getFiles method
    [KingGuppie] please test this as well

--- Cleanup_my_music/FileSystem.cs
+++ Cleanup_my_music/FileSystem.cs
@@ -15,10 +15,16 @@ namespace Cleanup_my_music {
         /// The allowed format, can easily expand this later
         /// </summary>
         private static string allowedExtentions = "*.mp3,*.flac,*.m4a,*.wav";
+
         /// <summary>
         /// The allowed format in a list, used for another implemtation
         /// </summary>
-        private static List<string> allowedExList = new List<string> { "*.mp3", "*.flac", "*.m4a", "*.wav" };
+        private static string[] allowedExList = { "*.mp3", "*.flac", "*.m4a", "*.wav" };
+
+        ///<summary>
+        /// Another list of extentions for the 3rd implementation
+        ///</summary>
+        private static string[] allowedExList0 = { ".mp3", ".flac", ".m4a", ".wav" };
 
         /// <summary>
         /// Get the files from param "root", with correct filters applied.
@@ -63,6 +69,38 @@ namespace Cleanup_my_music {
                     files0.AddRange(getFiles(d));
                 }
                 return files0;
+            }
+        }
+
+        /// <summary>
+        /// Yet another get files implementation
+        /// </summary>
+        /// <param name="root">Valid path to a root folder.</param>
+        /// <returns>A list containing all media file paths under the root folder</returns>
+        public static List<string> getFiles1(string 0){
+            string[] directories = Directory.GetDirectories(root);
+            //In this implementation I will use a removal approach instead of a filter approcah
+            string[] directories = Directory.GetDirectories(root);
+
+            List<string> files = Directory.GetFiles(root);
+            List<string> validFiles;
+
+            foreach(string f in files){
+                foreach(string ex in allowedExList0){
+                    if f.EndWith(ex){
+                        validFiles.Add(f);
+                        break;
+                    }    
+                }
+            }
+            
+            if (directories.Length <= 0) {
+                return validFiles;
+            } else {
+                foreach (string d in directories) {
+                    validFiles.AddRange(getFiles1(d));
+                }
+                return validFiles;
             }
         }
     }

8e9fcb2e5a5bd3f5e1ee82e7ed23cd8e81d5c41f
MaT1g3R <982341777@qq.com>
2017-01-22 17:42:47 -0500

    w

--- Cleanup_my_music/mainWindow.Designer.cs
+++ Cleanup_my_music/mainWindow.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(835, 601);
             this.Name = "mainWindow";
             this.Text = "Window";
             this.Load += new System.EventHandler(this.mainWindow_Load);
